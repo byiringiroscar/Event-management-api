080da13c063c7eaaee94d5ea52c520deaa2f8f82
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-10-04 00:20:37 +0200

    Update settings.py

--- event_management/settings.py
+++ event_management/settings.py
@@ -55,6 +55,7 @@ AUTH_USER_MODEL = 'authentication.User'
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
