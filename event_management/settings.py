b0bf2ba2c5017327b55de6124a93d879fcf495e0
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-10-03 20:21:18 +0200

    Add jwt and rest framework

--- event_management/settings.py
+++ event_management/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'authentication',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
