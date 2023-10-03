3c53cc0842a75a134e28cef8df53b5d0611727ea
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-10-03 21:03:23 +0200

    Add jwt

--- event_management/settings.py
+++ event_management/settings.py
@@ -42,6 +42,14 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
+
 AUTH_USER_MODEL = 'authentication.User'
 
 MIDDLEWARE = [
