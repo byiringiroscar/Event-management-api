a5c3e57276a920350746a9888c359c07da664f46
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-10-03 23:44:21 +0200

    Setup hosting stuff

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: python manage.py migrate && gunicorn <event_management>.wsgi
+
--- event_management/urls.py
+++ event_management/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -37,7 +41,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('api/', include('api.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     re_path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
