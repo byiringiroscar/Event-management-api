65dae039feb7272c93764c0bf6b2bf5d9f7b3b1f
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-10-10 21:18:41 +0200

    wip

--- photo_challenge/views.py
+++ photo_challenge/views.py
@@ -21,10 +21,14 @@ class PhotoChallengeList(ListCreateAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         if PhotoChallenge.objects.filter(user=self.request.user, event=serializer.validated_data['event']).exists():
-            return Response({"message": "You have already participated in this photo challenge"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "You have already participated in this photo challenge"},
+                            status=status.HTTP_400_BAD_REQUEST)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response({"message": "You have successfully participated in this photo challenge"}, status=status.HTTP_201_CREATED, headers=headers)
+        return Response({"message": "You have successfully participated in this photo challenge"},
+                        status=status.HTTP_201_CREATED, headers=headers)
+
+
 
 class PhotoChallengeDetail(RetrieveUpdateDestroyAPIView):
     queryset = PhotoChallenge.objects.all()
