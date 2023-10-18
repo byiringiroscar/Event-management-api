2644bbbc36bb3576faffac265fbc046522320772
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-10-18 10:16:12 +0200

    Add token verify endpoint

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, re_path
-from .views import RegistrationView
+from .views import RegistrationView, MyTokenObtainPairView
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
+    # TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView
 )
 
 
 urlpatterns = [
     path('register', RegistrationView.as_view(), name='register'),
-    path('event/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('event/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('event/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -6,8 +6,25 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
 from .renderers import UserRender
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 # Create your views here.
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 class RegistrationView(generics.GenericAPIView):
